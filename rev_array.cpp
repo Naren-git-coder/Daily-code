99b147899497c38bb0f1f7494e646798be791c1f
Naren-git-coder <narengit23@gmail.com>
2024-07-23 17:40:00 +0530

    rev_array.cpp

--- rev_array.cpp
+++ rev_array.cpp
@@ -1,41 +1,40 @@
 #include<iostream>
 using namespace std;
 
-int REV(int l, int r , int a[],int n )
-{
-	int temp = 0;
-	if(l<r)
-	{
-	temp = a[l];
-	a[l] = a[r];
-	a[r] = temp;
+void REV(int i, int n, int a[]) {
+    if(i >= n/2)
+        return;
+    int temp = a[i];
+    a[i] = a[n-i-1];
+    a[n-i-1] = temp;
+    REV(i + 1, n, a);
 }
-	REV(l+1,r-1,a,n);
-	for(int i = 0;i< n;i++)
-	{
-		cout<<a[i]<<" ";
-	}
-	
-}
-int main()
-{
-	int n,a[n],l,r;
-	cout<<"Enter the number of elements: ";
-	cin>>n;
-	for(int i = 0;i<n;i++)
-	{
-		cin>>a[i];
-	}
-	for(int i = 0;i<n;i++)
-	{
-		cout<<a[i]<<" ";
-	}
-	
-	REV(0,n-1,a,n);
-	
-	for(int i = 0;i<n;i++)
-	{
-		cout<<a[i]<<" ";
-	}
-	  
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+    
+    int a[n];
+    
+    cout << "Enter the elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+
+    cout << "Original array: ";
+    for(int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    
+    REV(0, n , a);
+    
+    cout << "Reversed array: ";
+    for(int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    
+    return 0;
 }
