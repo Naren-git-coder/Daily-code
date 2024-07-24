5c955d604ef8e40bfb34bfc73bf8fd029b1cd393
Naren-git-coder <narengit23@gmail.com>
2024-07-24 23:30:23 +0530

    Recursion
    Problem Statement :
    print the steps required for tower of hanoi of 3 pegs using recursion
    Complexity :
    The time complexity of the Tower of Hanoi algorithm is O(2^n) and the space complexity is O(n).

---
+++ Tower_of_hanoi.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void TOH(int n , int A , int B , int C)
+{
+	if(n>0)
+	{
+		TOH(n-1,A,C,B);
+		cout<<"Move from "<<A<<" to "<<C<<endl;
+		TOH(n-1,B,A,C);
+	}
+}
+
+int main()
+{
+	int n;
+	cout<<"Enter the number of discs: ";
+	cin>>n;
+	
+	TOH(n,1,2,3);
+}
